7609e6fd9ec4e36fd2adf144242c2a69472ec1cc
michalpom <michalpom@gmail.com>
2020-10-19 20:15:31 +0200

    Added solutions of Package lesson problems

---
+++ Problems/Getting input/src/Main.java
@@ -0,0 +1,9 @@
+class InputClass {
+    public static String getString() {
+
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        String text = scanner.nextLine();
+
+        return text;
+    }
+}
